a07bf26c9e10ffd92fc9feb68d41f0fc3c57c11e
Hsuan <hsuan.dev@gmail.com>
2021-07-13 21:56:52 +0800

    modify pwd pattern

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'account' => 'required|string|max:255|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
+            'password' => 'required|string|min:4',
         ]);
 
         $user = User::create([
