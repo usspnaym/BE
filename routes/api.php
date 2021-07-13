18962d66434cb7d0c36f5e2245f72470d4dfbebf
Hsuan <hsuan.dev@gmail.com>
2021-07-13 19:55:15 +0800

    fix typo

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::post('/auth/login', [\App\Http\Controllers\AuthController::class, 'login'
 Route::middleware(['auth:sanctum'])->get('/auth/me', [\App\Http\Controllers\AuthController::class, 'me']);
 Route::middleware(['auth:sanctum'])->post('/utils/uploadImage', [\App\Http\Controllers\UtilController::class, 'upload_image']);
 
-Route::apiResource('posts', \App\Http\Controllers\PostController::class . [
+Route::apiResource('posts', \App\Http\Controllers\PostController::class , [
     'only' => ['index', 'show', 'store', 'update', 'delete']
 ]);
 Route::apiResource('categories', \App\Http\Controllers\CategoryController::class, [
